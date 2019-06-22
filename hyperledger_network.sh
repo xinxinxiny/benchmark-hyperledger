14a35f38aac91f307d029beffc1f3dbfb2ad6126
xinxinxiny <49350773+xinxinxiny@users.noreply.github.com>
2019-06-21 23:40:39 -0400

    Update hyperledger_network.sh

--- hyperledger_network.sh
+++ hyperledger_network.sh
@@ -1,7 +1,6 @@
 #install cURL
 wget security.ubuntu.com/ubuntu
-echo install cURL DONE *********************************************************
-#intall docker
+echo install cURL DONE 
 sudo apt update
 sudo apt install --yes apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
@@ -9,36 +8,36 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debi
 sudo apt update
 sudo apt install --yes docker-ce
 sudo curl -sSL https://get.docker.com/ | sh
-echo install docker DONE ********************************************************
+echo install docker DONE 
 #install docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-echo install docker compose DONE **************************************************
+echo install docker compose DONE 
 #setting up Go language
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
-echo setting up Go language DONE *************************************************
+echo setting up Go language DONE 
 #install Node.js
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt install nodejs
 npm install npm@5.6.0 -g
-echo install Node.js DONE *******************************************************
+echo install Node.js DONE 
 #install python
 sudo apt-get install python
-echo install python DONE *******************************************************
+echo install python DONE 
 #install samples
 sudo apt-get install git
 git clone https://github.com/xinxinxiny/fabric-samples.git
 cd fabric-samples
 curl -sSL http://bit.ly/2ysbOFE | bash -s 1.2.1
 cd first-network
-echo install samples DONE ****************************************************
+echo install samples DONE 
 #generate network artifacts
 ./byfn.sh generate
-echo generate network artifacts DONE ****************************************
+echo generate network artifacts DONE 
 #bring up the network
 ./byfn.sh up
-echo bring up the network DONE *********************************************
+echo bring up the network DONE 
 #bring down the network
 ./byfn.sh down
-echo bring down the network DONE ******************************************
+echo bring down the network DONE 
